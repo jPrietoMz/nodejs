794d4ed4475efadb6ad8ff9fef88659bff14d15e
jPrietoMz <JPRIETOM392@alumnos.imf.com>
2024-03-06 12:56:30 +0100

    Partes 003, 004, 005, 006 y 007 completas
    actualización por parte del profesor

---
+++ 005-MongoDB/004-Insertar muchos elementos/001-Insertar muchos.js
@@ -0,0 +1,20 @@
+db.formularios.insertMany([
+    {
+    nombre:"Jose Manuel",
+    asunto:"Este es el segundo correo",
+    mensaje:"Este es el cuerpo del mensaje",
+    fecha:"2023-03-01"
+},
+    {
+    nombre:"Jose Manuel",
+    asunto:"Este es el tercer correo",
+    mensaje:"Este es el cuerpo del mensaje",
+    fecha:"2023-03-02"
+},
+    {
+    nombre:"Jose Manuel",
+    asunto:"Este es el cuarto correo",
+    mensaje:"Este es el cuerpo del mensaje",
+    fecha:"2023-03-03"
+}
+]);
